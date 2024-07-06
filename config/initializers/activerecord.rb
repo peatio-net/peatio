1ce146a9b7f37691b53710d3c83bd8f4a6f6224b
dnvu <dnvu.24h@gmail.com>
2024-07-06 15:37:25 +0700

    update format load config

--- config/initializers/activerecord.rb
+++ config/initializers/activerecord.rb
@@ -12,7 +12,7 @@ module ActiveRecord
 end
 
 Rails.configuration.database_support_json = \
-  ActiveRecord::Base.configurations[Rails.env]['support_json']
+  ActiveRecord::Base.configurations[Rails.env][:support_json]
 
 Rails.configuration.database_adapter = \
-  ActiveRecord::Base.configurations[Rails.env]['adapter']
+  ActiveRecord::Base.configurations[Rails.env][:adapter]
